9930753fe0f896b2be4e7afdefdd281c94e4e57c
Natalia Barnhart <natalia.barnhart@gmail.com>
2019-03-30 10:57:30 -0400

    Slower Clothes app, first commit.

---
+++ blog/models.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    focus = models.CharField(max_length=200)
+    low_price = models.IntegerField()
+    high_price = models.IntegerField()
+    text = models.TextField()
+    made_in = models.CharField(max_length=200)
+    based_in = models.CharField(max_length=200)
+    tags = models.CharField(max_length=200)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.company_name
