0b73f087c2af3849515a2a30623383dc2e8b517a
Natalia Barnhart <natalia.barnhart@gmail.com>
2019-04-13 23:30:57 -0400

    Added views to create/edit blog post inside the site. Other minor changes.

---
+++ blog/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+
+    class Meta:
+        model = Post
+        fields = ('company_name', 'website', 'text', 'focus', 'based_in', 'made_in', 'low_price', 'high_price', 'tags',)
